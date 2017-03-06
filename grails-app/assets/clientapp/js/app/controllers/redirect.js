9f198f7a8ea289f35cafa2934363d89d5a80ba8f
Michael Cavataio <mcavatai@oswego.edu>
2017-03-06 15:07:54 -0500

    Fixed cookies not getting cleared in Firefox

--- grails-app/assets/clientapp/js/app/controllers/redirect.js
+++ grails-app/assets/clientapp/js/app/controllers/redirect.js
@@ -2,7 +2,6 @@ angularApp
     //a controller to display the data to appropriately list views 
     // allows us to add data to the scope and access it from our views.
     .controller("DashboardRedirectController", function($scope, $window, $location, $routeParams, $timeout, currentUser) {
-        debugger
         if(!currentUser) {
             $window.location.href = '/logout'
         } else if(currentUser.type == 'student') {
