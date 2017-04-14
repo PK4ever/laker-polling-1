e29a6a1c224d1224650adc133716b6531c247904
zsabin <zsabin@gmail.com>
2017-04-14 14:40:44 -0400

    Updated Quiz Service with more generic helper functions to more easily implement future service methods

--- grails-app/services/edu/oswego/cs/lakerpolling/services/QuizService.groovy
+++ grails-app/services/edu/oswego/cs/lakerpolling/services/QuizService.groovy
@@ -15,72 +15,48 @@ class QuizService {
     CourseService courseService
 
     /**
-     * Endpoint to get a list of all of the question IDs for a quiz.
+     * Gets a list of all of the question IDs for a quiz.
      * @param token - The access token of the requesting user
      * @param quizIdString - the id of the quiz
      * @return a QueryResult containing the IDs of all of the questions in the given quiz
      */
     QueryResult<List<Long>> getAllQuestionIds(AuthToken token, String quizIdString) {
         QueryResult<List<Long>> result = new QueryResult<>()
-        def questionsResult = getAllQuestions(token, quizIdString)
-        if (!questionsResult.success) {
-            return QueryResult.copyError(questionsResult)
+        def quizResult = findQuiz(quizIdString)
+        if (!quizResult.success) {
+            return QueryResult.copyError(quizResult)
         }
-        result.data = questionsResult.data.collect { question -> question.id }
+
+        def quiz = quizResult.data
+        def accessCheck = verifyStudentAccess(token, quiz)
+        if (!accessCheck.success) {
+            return QueryResult.copyError(accessCheck)
+        }
+
+        result.data = quiz.questions.collect { question -> question.id }
         result
     }
 
     /**
-     * Endpoint to get the question with the given id of the specified quiz
+     * Gets the question with the given id of the specified quiz
      * @param access_token - The access token of the requesting user
      * @param quizIdString - the id of the quiz
      * @param questionIdString - the id of the question
      * @return a QueryResult containing the given question in the specified quiz
      */
     QueryResult<Question> getQuestion(AuthToken token, String quizIdString, String questionIdString) {
-        QueryResult<Question> result = new QueryResult<>()
-        if (!questionIdString.isLong()) {
-            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
-        }
-        def questionId = questionIdString.toLong()
-
-        def questionsResult = getAllQuestions(token, quizIdString)
-        if (!questionsResult.success) {
-            return QueryResult.copyError(questionsResult)
+        QueryResult<List<Long>> result = new QueryResult<>()
+        def quizResult = findQuiz(quizIdString)
+        if (!quizResult.success) {
+            return QueryResult.copyError(quizResult)
         }
 
-        def question = questionsResult.data.find { question -> question.id == questionId }
-        if (!question) {
-            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
+        def quiz = quizResult.data
+        def accessCheck = verifyStudentAccess(token, quiz)
+        if (!accessCheck.success) {
+            return QueryResult.copyError(accessCheck)
         }
-        result.data = question
-        result
-    }
-
-    /**
-     * Gets all questions for the given quiz
-     * @param access_token - The access token of the requesting user
-     * @param quizIdString - the id of the quiz
-     * @return a QueryResult containing a list of all of the questions for the given quiz
-     */
-    private QueryResult<List<Question>> getAllQuestions(AuthToken token, String quizIdString) {
-        QueryResult<List<Question>> result = new QueryResult<>()
-        User requestingUser = token?.user
-        if (!requestingUser || !quizIdString.isLong()) {
-            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
-        }
-
-        Quiz quiz = Quiz.findById(quizIdString.toLong())
-        if (!quiz) {
-            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
-        }
-
-        Course course = quiz.course
-        if(!courseService.hasStudentAccess(requestingUser, course)) {
-            return QueryResult.fromHttpStatus(HttpStatus.UNAUTHORIZED)
-        }
-        result.data = quiz.questions
-        result
+        findQuestion(quiz, questionIdString)
     }
 
     /**
@@ -94,25 +70,121 @@ class QuizService {
      */
     QueryResult<Question> postQuestionToQuiz(AuthToken token, String quizIdString, String text, String choiceString, String answerString) {
         QueryResult<Question> result = new QueryResult<>()
-        User requestingUser = token?.user
-        if (!requestingUser || !quizIdString.isLong()) {
-            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
-        }
-
         def choices = commaSeparatedStrToList(choiceString)
         def answers = toBooleanList(answerString)
-        Quiz quiz = Quiz.findById(quizIdString.toLong())
-        if (!quiz || !choices || !answers) {
+        if (!choices || !answers) {
             return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
         }
 
-        Course course = quiz.course
-        if (!courseService.hasInstructorAccess(requestingUser, course)) {
-            return QueryResult.fromHttpStatus(HttpStatus.UNAUTHORIZED)
+        def quizResult = findQuiz(quizIdString)
+        if (!quizResult.success) {
+            return QueryResult.copyError(quizResult)
         }
 
-        Question question = new Question(course: course, question: text, choices: choices, answers: answers)
+        def quiz = quizResult.data
+        def accessCheck = verifyInstructorAccess(token, quiz)
+        if (!accessCheck.success) {
+            return QueryResult.copyError(accessCheck)
+        }
+
+        Question question = new Question(course: quiz.course, question: text, choices: choices, answers: answers)
         quiz.addToQuestions(question)
+        result.data = question
+        result
+    }
+
+    /**
+     * Returns a successfull Query Result if the user represented in token has student access to the given quiz
+     * @param token - the Authtoken
+     * @param quiz - the quiz
+     * @return a QueryResult representing the result of the check
+     */
+    private QueryResult verifyStudentAccess(AuthToken token, Quiz quiz) {
+        def userResult = findUser(token)
+        if (!userResult.success) {
+            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
+        }
+
+        def user = userResult.data
+        def course = quiz.course
+        if (!courseService.hasStudentAccess(user, course)) {
+            return QueryResult.fromHttpStatus(HttpStatus.UNAUTHORIZED)
+        }
+        new QueryResult(success:true)
+    }
+
+    /**
+     * Returns a successful Query Result if the user represented in token has instructor access to the given quiz
+     * @param token - the Authtoken
+     * @param quiz - the quiz
+     * @return a QueryResult representing the result of the check
+     */
+    private QueryResult verifyInstructorAccess(AuthToken token, Quiz quiz) {
+        def userResult = findUser(token)
+        if (!userResult.success) {
+            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
+        }
+
+        def user = userResult.data
+        def course = quiz.course
+        if (!courseService.hasInstructorAccess(user, course)) {
+            return QueryResult.fromHttpStatus(HttpStatus.UNAUTHORIZED)
+        }
+        new QueryResult(success:true)
+    }
+
+    /**
+     * Attempts to find the user associated with the given AuthToken
+     * @param token - the AuthToken
+     * @return A QueryResult containing the associated user
+     */
+    private QueryResult<User> findUser(AuthToken token) {
+        QueryResult<Question> result = new QueryResult<>()
+        User user = token?.user
+        if (!user) {
+            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
+        }
+        result.data = user
+        result
+    }
+
+    /**
+     * Attempts to find the Quiz associated with the given ID String
+     * @param quizIdString - A String representing a Quiz ID
+     * @return A QueryResult containing the associated Quiz
+     */
+    private QueryResult<Quiz> findQuiz(String quizIdString) {
+        QueryResult<Quiz> result = new QueryResult<>()
+        if (!quizIdString.isLong()) {
+            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
+        }
+        Quiz quiz = Quiz.findById(quizIdString.toLong())
+        if (!quiz) {
+            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
+        }
+        result.data = quiz
+        result
+    }
+
+    /**
+     * Attempts to find the Question associated with the given quiz and question ID string
+     * @param quiz - the quiz
+     * @param questionIdString - a String representing a question ID
+     * @return A QueryResult containing the associated Question
+     */
+    private QueryResult<Question> findQuestion(Quiz quiz, String questionIdString) {
+        QueryResult<Question> result = new QueryResult<>()
+        if (!questionIdString.isLong()) {
+            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
+        }
+
+        def questionId = questionIdString.toLong()
+        Set<Question> questions = quiz.questions
+
+        def question = questions.find { question -> question.id == questionId }
+        if (!question) {
+            return QueryResult.fromHttpStatus(HttpStatus.BAD_REQUEST)
+        }
 
         result.data = question
         result
