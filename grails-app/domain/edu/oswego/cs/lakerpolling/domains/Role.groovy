da57a0d2b542ec367ee9ffab8d63b74ce3f32cd3
Jeff Registre <jeffreyregist365@gmail.com>
2017-04-16 20:40:54 -0400

    added validation for type and master role type.

--- grails-app/domain/edu/oswego/cs/lakerpolling/domains/Role.groovy
+++ grails-app/domain/edu/oswego/cs/lakerpolling/domains/Role.groovy
@@ -10,4 +10,19 @@ class Role {
     static belongsTo = [user: User]
     static mapping = {}
 
+    static constraints = {
+        master validator: { RoleType b, Role self ->
+            RoleType a = self.type
+            if (a == b) {
+                true
+            } else {
+                if (b == RoleType.ADMIN) {
+                    a == RoleType.INSTRUCTOR || a == RoleType.STUDENT
+                } else {
+                    a == RoleType.STUDENT
+                }
+            }
+        }
+    }
+
 }
