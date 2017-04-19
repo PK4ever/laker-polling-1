6e362eb52d2c5e54d41804a703749bb894e41edf
Mateusz Wiater <mateuszwiater@gmail.com>
2017-04-19 18:04:44 -0400

    Tests for CourseListParserService

---
+++ src/test/groovy/edu/oswego/cs/lakerpolling/services/CourseListParserServiceSpec.groovy
@@ -0,0 +1,64 @@
+package edu.oswego.cs.lakerpolling.services
+
+import edu.oswego.cs.lakerpolling.util.QueryResult
+import grails.test.mixin.TestFor
+import org.springframework.mock.web.MockMultipartFile
+import spock.lang.Specification
+
+@TestFor(CourseListParserService)
+class CourseListParserServiceSpec extends Specification {
+
+    def "Test Email With Header"() {
+        when: "The CSV is loaded"
+        QueryResult<List<String>> users = service.parse(getFile("EmailWithHeader.csv"))
+
+        then: "There should be the following users"
+        users.data.contains("user1@oswego.edu")
+        users.data.contains("user2@oswego.edu")
+        users.data.contains("user3@oswego.edu")
+    }
+
+    def "Test Email Without Header"() {
+        when: "The CSV is loaded"
+        QueryResult<List<String>> users = service.parse(getFile("EmailWithoutHeader.csv"))
+
+        then: "There should be the following users"
+        users.data.contains("user1@oswego.edu")
+        users.data.contains("user2@oswego.edu")
+        users.data.contains("user3@oswego.edu")
+    }
+
+    def "Test Username With Header"() {
+        when: "The CSV is loaded"
+        QueryResult<List<String>> users = service.parse(getFile("UsernameWithHeader.csv"))
+
+        then: "There should be the following users"
+        users.data.contains("user1@oswego.edu")
+        users.data.contains("user2@oswego.edu")
+        users.data.contains("user3@oswego.edu")
+    }
+
+    def "Test Different Delimiter"() {
+        when: "The CSV is loaded"
+        QueryResult<List<String>> users = service.parse(getFile("DifferentDelimiter.csv"))
+
+        then: "There should be no users"
+        !users.success
+    }
+
+    def "Test Bad Email"() {
+        when: "The CSV is loaded"
+        QueryResult<List<String>> users = service.parse(getFile("BadEmailWithHeader.csv"))
+
+        then: "There should be the following users"
+        users.data.contains("user1@oswego.edu")
+        users.data.contains("user3@oswego.edu")
+
+        then: "And not the following user"
+        !users.data.contains("user2@gmail.com")
+    }
+
+    private MockMultipartFile getFile(String name) {
+        return new MockMultipartFile(name,this.getClass().getClassLoader().getResourceAsStream("courseListParserServiceSpec/$name"))
+    }
+}
