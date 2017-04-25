3a239df5d0eb1cbc65e63d77f83edae2e9f122b3
Mateusz Wiater <mateuszwiater@gmail.com>
2017-04-25 13:36:30 -0400

    Added missing testing classes.

---
+++ src/test/groovy/edu/oswego/cs/lakerpolling/controllers/QuizControllerSpec.groovy
@@ -0,0 +1,19 @@
+package edu.oswego.cs.lakerpolling.controllers
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(QuizController)
+class QuizControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+        true == false
+    }
+}
