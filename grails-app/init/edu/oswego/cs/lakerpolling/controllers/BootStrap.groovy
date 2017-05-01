352bbc3b12ffda41a9f1c962104d752e2d9b2597
Jeff Registre <jeffreyregist365@gmail.com>
2017-05-01 16:36:37 -0400

    Added one user to bootstrap.

--- grails-app/init/edu/oswego/cs/lakerpolling/controllers/BootStrap.groovy
+++ grails-app/init/edu/oswego/cs/lakerpolling/controllers/BootStrap.groovy
@@ -1,8 +1,19 @@
 package edu.oswego.cs.lakerpolling.controllers
 
+import edu.oswego.cs.lakerpolling.domains.Role
+import edu.oswego.cs.lakerpolling.domains.User
+import edu.oswego.cs.lakerpolling.util.RoleType
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        if (!User.findByEmail("bastian.tenbergen@oswego.edu")) {
+            User user = new User(email: "bastian.tenbergen@oswego.edu")
+            user.setRole(new Role(type: RoleType.INSTRUCTOR, master: RoleType.INSTRUCTOR))
+            user.save(flush:true)
+        }
+
     }
 
     def destroy = {
